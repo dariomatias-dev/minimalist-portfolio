5f0fa0509f5e547f2845a536deb2dbe4d313cec8
dariomatias-dev <matiasdario75@gmail.com>
2025-06-23 06:09:40 -0300

    feat(src): componentized About Me section
    - Componentized the About Me section to separate concerns and improve structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { AboutMeSection } from "@/components/AboutMeSection";
 import { ContactSection } from "@/components/ContactSection";
 import { EducationSection } from "@/components/EducationSection";
 import { ExperienceSection } from "@/components/ExperienceSection";
@@ -12,25 +13,7 @@ export default function Home() {
       <Header />
 
       <main className="mx-auto min-h-screen max-w-5xl bg-white px-6 py-16 text-gray-900">
-        {/* About Me Section */}
-        <section
-          id="about"
-          className="mx-auto mb-20 max-w-5xl leading-relaxed text-gray-800"
-        >
-          <h2 className="mb-6 border-b border-gray-300 pb-2 text-3xl font-semibold">
-            Sobre Mim
-          </h2>
-
-          <p>
-            Sou um desenvolvedor Full Stack com ampla experiência no
-            desenvolvimento de sites, servidores e aplicativos móveis. Tenho
-            conhecimento em tecnologias Web modernas, especialmente React e
-            Next.js, focando na criação de interfaces intuitivas e responsivas,
-            incluindo Flutter para o desenvolvimento de aplicativos móveis
-            eficientes. Busco sempre entregar soluções com código limpo,
-            desempenho otimizado e excelente experiência para o usuário final.
-          </p>
-        </section>
+        <AboutMeSection />
 
         <ProjectsSection />
 
---
+++ src/components/AboutMeSection.tsx
@@ -0,0 +1,22 @@
+export const AboutMeSection = () => {
+  return (
+    <section
+      id="about"
+      className="mx-auto mb-20 max-w-5xl leading-relaxed text-gray-800"
+    >
+      <h2 className="mb-6 border-b border-gray-300 pb-2 text-3xl font-semibold">
+        Sobre Mim
+      </h2>
+
+      <p>
+        Sou um desenvolvedor Full Stack com ampla experiência no desenvolvimento
+        de sites, servidores e aplicativos móveis. Tenho conhecimento em
+        tecnologias Web modernas, especialmente React e Next.js, focando na
+        criação de interfaces intuitivas e responsivas, incluindo Flutter para o
+        desenvolvimento de aplicativos móveis eficientes. Busco sempre entregar
+        soluções com código limpo, desempenho otimizado e excelente experiência
+        para o usuário final.
+      </p>
+    </section>
+  );
+};
