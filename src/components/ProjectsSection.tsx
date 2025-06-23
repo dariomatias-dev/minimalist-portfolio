ec2b6c65cde40994997dc883c8c49e0c50ff521f
dariomatias-dev <matiasdario75@gmail.com>
2025-06-23 17:41:11 -0300

    feat(src/components/ProjectsSection.tsx): added main projects
    - Added main projects for display.

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -14,27 +14,60 @@ import {
 
 const projects = [
   {
-    title: "Flutter Guide",
-    description: "App educativo para aprender Flutter com exemplos práticos.",
+    title: "Flutter Guide App",
+    description:
+      "Flutter Guide é um app educativo que ajuda desenvolvedores a aprender Flutter com exemplos práticos.",
     technologies: ["Dart", "Flutter"],
     image:
       "https://raw.githubusercontent.com/dariomatias-dev/flutter_guide_web/main/public/flutter_guide_icon.png",
     links: {
       playStore:
-        "https://play.google.com/store/apps/details?id=com.example.flutterguide",
-      github: "https://github.com/dariomatias-dev/flutter_guide",
+        "https://play.google.com/store/apps/details?id=com.dariomatias.flutter_guide",
+      github: "https://github.com/dariomatias-dev/flutter_guide_app",
     },
   },
   {
-    title: "Portfolio Next.js",
+    title: "Flutter Guide Web",
     description:
-      "Meu portfólio pessoal construído com Next.js, Tailwind e shadcn/ui.",
+      "Site oficial do Flutter Guide, com informações, principais funcionalidades, capturas de tela, política de privacidade e link direto para download na Google Play Store.",
     technologies: ["TailwindCSS", "TypeScript", "Next.js", "shadcn/ui"],
     image:
       "https://raw.githubusercontent.com/dariomatias-dev/flutter_guide_web/main/public/flutter_guide_icon.png",
     links: {
-      site: "https://dariomatias.dev",
-      github: "https://github.com/dariomatias-dev/portfolio",
+      site: "https://flutter-guide-web.vercel.app/",
+      github: "https://github.com/dariomatias-dev/flutter_guide_web",
+    },
+  },
+  {
+    title: "Fit App",
+    description: "Aplicativo de treinos para facilitar seus exercícios.",
+    technologies: ["Dart", "Flutter"],
+    image:
+      "https://raw.githubusercontent.com/dariomatias-dev/flutter_guide_web/main/public/flutter_guide_icon.png",
+    links: {
+      playStore:
+        "https://play.google.com/store/apps/details?id=com.dariomatias.fit_app",
+    },
+  },
+  {
+    title: "Scroll Infinity",
+    description: "Pacote Flutter para adição simplificada de rolagem infinita.",
+    technologies: ["Dart", "Flutter"],
+    image:
+      "https://raw.githubusercontent.com/dariomatias-dev/flutter_guide_web/main/public/flutter_guide_icon.png",
+    links: {
+      github: "https://github.com/dariomatias-dev/scroll_infinity",
+    },
+  },
+  {
+    title: "Go Validators",
+    description:
+      "Pacote com utilitários de validação para garantir a integridade dos dados em aplicações Go, utilizando apenas bibliotecas nativas.",
+    technologies: ["GoLang"],
+    image:
+      "https://raw.githubusercontent.com/dariomatias-dev/flutter_guide_web/main/public/flutter_guide_icon.png",
+    links: {
+      github: "https://github.com/dariomatias-dev/go-validators",
     },
   },
 ];
