a252361d721cff7fe9487712e287a22b736f0292
dariomatias-dev <matiasdario75@gmail.com>
2025-07-08 19:13:03 -0300

    feat: added internationalization to not found page
    - Implemented support for multiple languages on the not found page.

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -2,10 +2,13 @@
 
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
+  const t = useTranslations("NotFoundPage");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-6 text-center">
       <div className="max-w-md">
@@ -13,12 +16,11 @@ export default function NotFound() {
           404
         </h1>
         <h2 className="mt-4 text-2xl font-semibold text-gray-800 dark:text-zinc-200">
-          Página não encontrada
+          {t("title")}
         </h2>
-        <p className="mt-2 text-base text-gray-600 dark:text-zinc-300">
-          Desculpe, não conseguimos encontrar a página que você está procurando.
-          Ela pode ter sido removida, renomeada ou estar temporariamente
-          indisponível.
+        
+        <p>
+          {t("description")}
         </p>
 
         <Link href="/" className="mt-8 inline-block">
@@ -27,7 +29,7 @@ export default function NotFound() {
             className="group inline-flex min-w-[200px] items-center justify-center gap-2 rounded-full border border-gray-300 bg-transparent py-6 text-sm font-medium text-gray-800 transition-colors duration-150 hover:bg-gray-50 focus-visible:ring-1 focus-visible:ring-gray-300 focus-visible:outline-none dark:border-gray-100 dark:text-zinc-200"
           >
             <ArrowLeft className="h-5 w-5 transition-transform duration-200 group-hover:-translate-x-1 group-focus-visible:-translate-x-1" />
-            Voltar à página inicial
+            {t("button")}
           </Button>
         </Link>
       </div>
